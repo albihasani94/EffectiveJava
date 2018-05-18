fbae195661fcaabe98de14f435a257a2c5378f15
ahasani <albin.hasani@ikubinfo.al>
2018-05-19 00:44:25 +0200

    Item 1: Reorder some code fragments
      - move static variable declarations before the constructor
      - move the print statements under their declarations

--- src/chapter1/item1/Application.java
+++ src/chapter1/item1/Application.java
@@ -7,11 +7,10 @@ public class Application {
 
         //It is harder to know the parameters order
         WagonTrackingData wagonTrackingDataViaConstructor = new WagonTrackingData("testdest", "testOrigin", "testCurrent");
+        System.out.println(wagonTrackingDataViaConstructor);
 
         //It is now easier to describe the parameters order in the name of the static factory method
         WagonTrackingData wagonTrackingDataViaStaticFactory = WagonTrackingData.ofCurrentAndOriginAndDestinationLocation("tirane", "sarande", "Mars");
-
-        System.out.println(wagonTrackingDataViaConstructor);
         System.out.println(wagonTrackingDataViaStaticFactory);
 
 
--- src/chapter1/item1/Evaluation.java
+++ src/chapter1/item1/Evaluation.java
@@ -4,17 +4,17 @@ public class Evaluation {
 
     private int value;
 
+    public static final Evaluation ONE = new Evaluation(true);
+
+    public static Evaluation ZERO = new Evaluation(false);
+
     public Evaluation(boolean bol) {
         System.out.println(String.format("Calling constructor with boolean %b", bol));
         this.value = bol ? 1 : 0;
     }
 
-    public static final Evaluation ONE = new Evaluation(true);
-
-    public static Evaluation ZERO = new Evaluation(false);
-
     public static Evaluation fromBooleanToInt(boolean bol) {
         return bol ? Evaluation.ONE : Evaluation.ZERO;
     }
-    
+
 }
