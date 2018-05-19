bca466f675fce619466513b707e551e158754f55
ahasani <albin.hasani@ikubinfo.al>
2018-05-20 00:56:17 +0200

    Item 2: Implement the builder pattern

--- src/chapter1/item2/Application.java
+++ src/chapter1/item2/Application.java
@@ -1,5 +1,17 @@
 package chapter1.item2;
 
+import java.time.LocalDate;
+
 //Item 2: Consider a builder when faces with many constructor parameters
 public class Application {
+
+    public static void main(String[] args) {
+        Book book = new Book.Builder("Effective Java 3rd Edition")
+                .withAuthor("Joshua Bloch")
+                .withReleaseDate(LocalDate.of(2018, 1, 1))
+                .build();
+
+        System.out.println(book);
+    }
+
 }
--- src/chapter1/item2/Book.java
+++ src/chapter1/item2/Book.java
@@ -13,14 +13,37 @@ public class Book {
         private String author;
         private LocalDate releaseDate;
 
-        public Builder(String title, String author) {
+        public Builder(String title) {
             this.title = title;
+        }
+
+        public Builder withAuthor(String author) {
             this.author = author;
+            return this;
         }
 
         public Builder withReleaseDate(LocalDate releaseDate) {
             this.releaseDate = releaseDate;
             return this;
         }
+
+        public Book build() {
+            return new Book(this);
+        }
+    }
+
+    private Book(Builder builder) {
+        title = builder.title;
+        author = builder.author;
+        releaseDate = builder.releaseDate;
+    }
+
+    @Override
+    public String toString() {
+        return "Book{" +
+                "title='" + title + '\'' +
+                ", author='" + author + '\'' +
+                ", releaseDate=" + releaseDate +
+                '}';
     }
 }
